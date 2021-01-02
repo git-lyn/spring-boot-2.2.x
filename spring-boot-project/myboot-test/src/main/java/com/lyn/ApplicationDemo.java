2e9b5838b4a7bb47d9ed93a9bc1b5950b42ccc09
lyn-git <1415883718@qq.com>
2021-01-02 19:09:28 +0800

    master branch

--- spring-boot-project/myboot-test/src/main/java/com/lyn/ApplicationDemo.java
+++ spring-boot-project/myboot-test/src/main/java/com/lyn/ApplicationDemo.java
@@ -12,5 +12,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApplicationDemo {
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationDemo.class, args);
+		System.out.println("master....");
 	}
 }
