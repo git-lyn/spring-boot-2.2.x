ede97e1a430d4e660b407e0ed34de05898107637
lyn-git <1415883718@qq.com>
2021-01-02 18:56:30 +0800

    spring-boot-2.2.x源码注解

---
+++ spring-boot-project/myboot-test/src/main/java/com/lyn/ApplicationDemo.java
@@ -0,0 +1,16 @@
+package com.lyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: spring-boot-build-2.2.x
+ * @author: lyn
+ * * @create: 2021-01-02 17:32
+ **/
+@SpringBootApplication
+public class ApplicationDemo {
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationDemo.class, args);
+	}
+}
